fc124692a47f98d106de2b25f03d373087bf9d09
alyxshang <179976644+alyxshang@users.noreply.github.com>
2025-03-15 17:10:41 +0100

    Code formatting.

--- lua/crimsononi/theme.lua
+++ lua/crimsononi/theme.lua
@@ -29,7 +29,7 @@ local function setEditorTheme()
 		"MsgArea", 
 		{ 
 			fg = colors.derivatives.base2, 
-			bg = colors.derivatives.base3
+			bg = colors.derivatives.base1
 		}
 	)
   	hl(
@@ -88,60 +88,435 @@ local function setEditorTheme()
 			underline=true,
 		}
 	)
-  	hl(0, "NormalNC", { fg = colors.derivatives.base2, bg = colors.derivatives.base1 })
-  	hl(0, "Pmenu", { fg = colors.derivatives.base11, bg = colors.derivatives.base3 })
-  	hl(0, "PmenuSel", { fg = 'NONE', bg = colors.fixed.blue })
-  	hl(0, "WildMenu", { fg = colors.derivatives.base2, bg = colors.fixed.blue })
-  	hl(0, "CursorLineNr", { fg = colors.derivatives.base5, bg = colors.derivatives.base2, bold=true, })
-  	hl(0, "Folded", { fg = colors.fixed.orange, bg = colors.derivatives.base3 })
-  	hl(0, "FoldColumn", { fg = colors.derivatives.base11, bg = colors.derivatives.base3 })
-  	hl(0, "LineNr", { fg = colors.fixed.red, bg = colors.derivatives.base1 })
-  	hl(0, "FloatBoder", { fg = colors.fixed.orange, bg = colors.derivatives.base3 })
-  	hl(0, "Whitespace", { fg = colors.derivatives.base1, bg = 'NONE' })
-  	hl(0, "VertSplit", { fg = colors.derivatives.orange, bg = colors.derivatives.base1 })
-  	hl(0, "CursorLine", { fg = 'NONE', bg = colors.derivatives.base2 })
-  	hl(0, "CursorColumn", { fg = 'NONE', bg = colors.derivatives.base2 })
-  	hl(0, "ColorColumn", { fg = 'NONE', bg = colors.derivatives.base3 })
-  	hl(0, "NormalFloat", { fg = 'NONE', bg = colors.derivatives.base3 })
-  	hl(0, "Visual", { fg = 'NONE', bg = colors.derivatives.base3 })
-  	hl(0, "VisualNOS", { fg = 'NONE', bg = colors.derivatives.base3 })
-  	hl(0, "WarningMsg", { fg = c.error, bg = colors.derivatives.base1 })
-  	hl(0, "DiffText", { fg = colors.derivatives.base3, bg = colors.fixed.light_red })
-  	hl(0, "DiffAdd", { fg = colors.derivatives.base3, bg = colors.fixed.light_yellow })
-  	hl(0, "DiffChange", { fg = colors.derivatives.base3, bg = colors.fixed.light_green, underline=true, })
-  	hl(0, "DiffDelete", { fg = colors.derivatives.base3, bg = colors.fixed.light_red })
-  	hl(0, "QuickFixLine", { fg = 'NONE', bg = colors.fixed.blue })
-  	hl(0, "PmenuSbar", { fg = 'NONE', bg = colors.derivatives.base3 })
-  	hl(0, "PmenuThumb", { fg = 'NONE', bg = colors.derivatives.base3 })
-  	hl(0, "MatchWord", { fg = 'NONE', bg = 'NONE', underline=true, })
-  	hl(0, "MatchParen", { fg = colors.fixed.yellow, bg = 'NONE', underline=true, })
-  	hl(0, "MatchWordCur", { fg = 'NONE', bg = 'NONE', underline=true, })
-  	hl(0, "MatchParenCur", { fg = 'NONE', bg = 'NONE', underline=true, })
-  hl(0, "Cursor", { fg = colors.derivatives.base2, bg = colors.derivatives.base6 })
-  hl(0, "lCursor", { fg = colors.derivatives.base2, bg = colors.derivatives.base6 })
-  hl(0, "CursorIM", { fg = colors.derivatives.base2, bg = colors.derivatives.base6 })
-  hl(0, "TermCursor", { fg = colors.derivatives.base2, bg = colors.derivatives.base6 })
-  hl(0, "TermCursorNC", { fg = colors.derivatives.base1, bg = colors.derivatives.base6 })
-  	hl(0, "Conceal", { fg = colors.fixed.cyan, bg = 'NONE' })
-  	hl(0, "Directory", { fg = colors.fixed.magenta, bg = 'NONE' })
-  	hl(0, "SpecialKey", { fg = colors.fixed.red, bg = 'NONE', bold=true, })
-  	hl(0, "ErrorMsg", { fg = colors.fixed.red, bg = colors.derivatives.base1, bold=true, })
-  	hl(0, "Search", { fg = colors.fixed.light_red, bg = colors.fixed.yellow })
-  	hl(0, "IncSearch", { fg = 'NONE', bg = 'NONE', reverse=true, })
-  	hl(0, "Substitute", { fg = 'NONE', bg = colors.derivatives.base5 })
-  	hl(0, "MoreMsg", { fg = colors.derivatives.base4, bg = 'NONE' })
-  	hl(0, "Question", { fg = colors.derivatives.base4, bg = 'NONE' })
-  	hl(0, "EndOfBuffer", { fg = colors.derivatives.base2, bg = 'NONE' })
-  	hl(0, "NonText", { fg = colors.derivatives.base1, bg = 'NONE' })
-  	hl(0, "TabLine", { fg = colors.derivatives.base3, bg = colors.derivatives.base2 })
-  	hl(0, "TabLineSel", { fg = colors.derivatives.base2, bg = colors.derivatives.base1 })
-  	hl(0, "TabLineFill", { fg = colors.derivatives.base2, bg = colors.derivatives.base1 })
+  	hl(
+		0, 
+		"NormalNC", 
+		{ 
+			fg = colors.derivatives.base2, 
+			bg = colors.derivatives.base1
+		}
+	)
+  	hl(
+		0, 
+		"Pmenu", 
+		{
+			fg = colors.derivatives.base11, 
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0, 
+		"PmenuSel", 
+		{
+			fg = 'NONE', 
+			bg = colors.fixed.blue 
+		}
+	)
+  	hl(
+		0, 
+		"WildMenu", 
+		{ 
+			fg = colors.derivatives.base2, 
+			bg = colors.fixed.blue
+		}
+	)
+  	hl(
+		0, 
+		"CursorLineNr", 
+		{
+			fg = colors.derivatives.base5, 
+			bg = colors.derivatives.base2, 
+			bold=true
+		}
+	)
+  	hl(
+		0, 
+		"Folded", 
+		{ 
+			fg = colors.fixed.orange, 
+			bg = colors.derivatives.base3 
+		}
+	)
+  	hl(
+		0,
+		"FoldColumn",
+		{
+			fg = colors.derivatives.base11, 
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0, 
+		"LineNr",
+		{
+			fg = colors.fixed.red, 
+			bg = colors.derivatives.base1
+		}
+	)
+  	hl(
+		0, 
+		"FloatBoder", 
+		{
+			fg = colors.fixed.orange, 
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0, 
+		"Whitespace",
+		{ 
+			fg = colors.derivatives.base1, 
+			bg = 'NONE'
+		}
+	)
+  	hl(
+		0, 
+		"VertSplit",
+		{ 
+			fg = colors.derivatives.orange, 
+			bg = colors.derivatives.base1 
+		}
+	)
+  	hl(
+		0, "CursorLine",
+		{
+			fg = 'NONE', 
+			bg = colors.derivatives.base2
+		}
+	)
+  	hl(
+		0, 
+		"CursorColumn",
+		{
+			fg = 'NONE', 
+			bg = colors.derivatives.base2
+		}
+	)
+  	hl(
+		0, 
+		"ColorColumn",
+		{
+			fg = 'NONE', 
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0, 
+		"NormalFloat", 
+		{
+			fg = 'NONE', 
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0,
+		"Visual",
+		{
+			fg = 'NONE', 
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0,
+		"VisualNOS",
+		{
+			fg = 'NONE',
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0,
+		"WarningMsg",
+		{
+			fg = colors.fixed.red, 
+			bg = colors.derivatives.base1
+		}
+	)
+  	hl(
+		0,
+		"DiffText",
+		{
+			fg = colors.derivatives.base3, 
+			bg = colors.fixed.light_red
+		}
+	)
+  	hl(
+		0,
+		"DiffAdd",
+		{
+			fg = colors.derivatives.base3,
+			bg = colors.fixed.light_yellow
+		}
+	)
+  	hl(
+		0,
+		"DiffChange",
+		{
+			fg = colors.derivatives.base3, 
+			bg = colors.fixed.light_green, 
+			underline=true,
+		}
+	)
+  	hl(
+		0,
+		"DiffDelete",
+		{
+			fg = colors.derivatives.base3,
+			bg = colors.fixed.light_red
+		}
+	)
+  	hl(
+		0,
+		"QuickFixLine",
+		{
+			fg = 'NONE',
+			bg = colors.fixed.blue
+		}
+	)
+  	hl(
+		0,
+		"PmenuSbar",
+		{
+			fg = 'NONE',
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0, 
+		"PmenuThumb",
+		{
+			fg = 'NONE',
+			bg = colors.derivatives.base3
+		}
+	)
+  	hl(
+		0,
+		"MatchWord",
+		{
+			fg = 'NONE', 
+			bg = 'NONE', 
+			underline=true,
+		}
+	)
+  	hl(
+		0,
+		"MatchParen",
+		{
+			fg = colors.fixed.yellow,
+			bg = 'NONE', underline=true,
+		}
+	)
+  	hl(
+		0,
+		"MatchWordCur",
+		{ 
+			fg = 'NONE',
+			bg = 'NONE', 
+			underline=true,
+		}
+	)
+  	hl(
+		0,
+		"MatchParenCur",
+		{
+			fg = 'NONE',
+			bg = 'NONE',
+			underline=true,
+		}
+	)
+  	hl(
+		0,
+		"Cursor",
+		{
+			fg = colors.derivatives.base2, 
+			bg = colors.derivatives.base6
+		}
+	)
+  	hl(
+		0,
+		"lCursor",
+		{
+			fg = colors.derivatives.base2,
+			bg = colors.derivatives.base6
+		}
+	)
+  	hl(
+		0, 
+		"CursorIM",
+		{
+			fg = colors.derivatives.base2,
+			bg = colors.derivatives.base6
+		}
+	)
+  	hl(
+		0, 
+		"TermCursor", 
+		{
+			fg = colors.derivatives.base2, 
+			bg = colors.derivatives.base6
+		}
+	)
+  	hl(
+		0,
+		"TermCursorNC",
+		{
+			fg = colors.derivatives.base1, 
+			bg = colors.derivatives.base6
+		}
+	)
+  	hl(
+		0,
+		"Conceal",
+		{
+			fg = colors.fixed.cyan, 
+			bg = 'NONE'
+		}
+	)
+  	hl(
+		0, 
+		"Directory",
+		{
+			fg = colors.fixed.magenta,
+			bg = 'NONE'
+		}
+	)
+  	hl(
+		0, 
+		"SpecialKey",
+		{
+			fg = colors.fixed.red, 
+			bg = 'NONE', 
+			bold=true,
+		}
+	)
+  	hl(
+		0,
+		"ErrorMsg",
+		{
+			fg = colors.fixed.red,
+			bg = colors.derivatives.base1,
+			bold=true, 
+		}
+	)
+  	hl(
+		0, 
+		"Search",
+		{
+			fg = colors.fixed.light_red, 
+			bg = colors.fixed.yellow
+		}
+	)
+  	hl(
+		0,
+		"IncSearch",
+		{
+			fg = 'NONE', 
+			bg = 'NONE', 
+			reverse=true,
+		}
+	)
+  	hl(
+		0,
+		"Substitute",
+		{
+			fg = 'NONE',
+			bg = colors.derivatives.base5
+		}
+	)
+  	hl(
+		0, 
+		"MoreMsg",
+		{
+			fg = colors.derivatives.base4, 
+			bg = 'NONE'
+		}
+	)
+  	hl(
+		0, 
+		"Question",
+		{
+			fg = colors.derivatives.base4, 
+			bg = 'NONE'
+		}
+	)
+  	hl(
+		0, 
+		"EndOfBuffer", 
+		{
+			fg = colors.derivatives.base2, 
+			bg = 'NONE'
+		}
+	)
+  	hl(
+		0,
+		"NonText",
+		{
+			fg = colors.derivatives.base1, 
+			bg = 'NONE'
+		}
+	)
+  	hl(
+		0,
+		"TabLine",
+		{
+			fg = colors.derivatives.base3,
+			bg = colors.derivatives.base2
+		}
+	)
+  	hl(
+		0, 
+		"TabLineSel",
+		{
+			fg = colors.derivatives.base2, 
+			bg = colors.derivatives.base1
+		}
+	)
+  	hl(
+		0, 
+		"TabLineFill",
+		{
+			fg = colors.derivatives.base2, 
+			bg = colors.derivatives.base1 
+		}
+	)
 end
 
 -- A function to set the theme
 -- for the "status" bar provided
 -- by the "lualine" plugin.
 local function setLuaLineTheme()
+	return {
+		normal = {
+		  a = {bg = colors.gray, fg = colors.black, gui = 'bold'},
+		  b = {bg = colors.lightgray, fg = colors.white},
+		  c = {bg = colors.darkgray, fg = colors.gray}
+		},
+		insert = {
+		  a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+		  b = {bg = colors.lightgray, fg = colors.white},
+		  c = {bg = colors.lightgray, fg = colors.white}
+		},
+		visual = {
+		  a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+		  b = {bg = colors.lightgray, fg = colors.white},
+		  c = {bg = colors.inactivegray, fg = colors.black}
+		},
+		replace = {
+		  a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+		  b = {bg = colors.lightgray, fg = colors.white},
+		  c = {bg = colors.black, fg = colors.white}
+		},
+		command = {
+		  a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+		  b = {bg = colors.lightgray, fg = colors.white},
+		  c = {bg = colors.inactivegray, fg = colors.black}
+		},
+		inactive = {
+		  a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
+		  b = {bg = colors.darkgray, fg = colors.gray},
+		  c = {bg = colors.darkgray, fg = colors.gray}
+		}
+	}
 end
 
 -- A function to set the theme for
